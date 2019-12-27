9851fb34240c58ae81be569bd0148672749543f6
Victor Canta <proyectos.ini.2019@gmail.com>
2019-12-27 12:16:20 -0500

    Creacion de Header - Boton retroceso funcional - icono login

--- android/app/src/main/java/com/itnovarte/bellmoi/CameraActivity.java
+++ android/app/src/main/java/com/itnovarte/bellmoi/CameraActivity.java
@@ -3,9 +3,11 @@ package com.itnovarte.bellmoi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
 
 public class CameraActivity extends AppCompatActivity {
 
@@ -16,13 +18,28 @@ public class CameraActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
+        toolbar.setNavigationIcon(R.mipmap.abc_ic_ab_back_mtrl_am_alpha);
+/*
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });*/
     }
 
-    //@Override
-    public boolean onCreationOptionsMenu(Menu menu){
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
+    @Override
+    public boolean onSupportNavigateUp () {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
         return true;
     }
 
