24563b34f0f62d5eb9abd82fadd740511e474ccb
Victor Canta <proyectos.ini.2019@gmail.com>
2019-12-27 05:03:48 -0500

    Creacion del Header.

--- android/app/src/main/java/com/itnovarte/bellmoi/CameraActivity.java
+++ android/app/src/main/java/com/itnovarte/bellmoi/CameraActivity.java
@@ -1,8 +1,11 @@
 package com.itnovarte.bellmoi;
 
-//import android.support.v7.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class CameraActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class CameraActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
+
+    //@Override
+    public boolean onCreationOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
 }
